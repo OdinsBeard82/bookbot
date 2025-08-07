3982c41ecd07449ebc27ed46ba7ca389206f753d
Daniel Charles <danielcharles_500@hotmail.com>
2025-08-07 12:43:37 +0100

    Print a Report lesson complete

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, character_count
+from stats import count_words, character_count, print_a_report
 
 def get_book_text(path):
     with open(path) as f:
@@ -7,10 +7,16 @@ def get_book_text(path):
 def main():
     result = get_book_text("books/frankenstein.txt")
     num_words = count_words(result)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
 
     char_counts = character_count(result)
-    print(char_counts)
+
+    report = print_a_report(char_counts)
+    
+    for item in report:
+        print(f"{item['char']}: {item['num']}")
+
+
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -11,3 +11,16 @@ def character_count(text):
         else:
             char_count[char] = 1 # If it’s not in the dictionary, count + 1
     return char_count # Return the dictionary
+
+def print_a_report(char_count):
+    sorted_list = []  # Empty list to store dictionaries of characters and counts
+
+    for char, count in char_count.items():  # Loop through the dictionary
+        if not char.isalpha():  # Skip characters that are not letters
+            continue
+        sorted_list.append({"char": char, "num": count})  # Add a dictionary with char and count
+
+    sorted_list.sort(key=lambda d: d["num"], reverse=True)  # Sort list from greatest to least by count
+    return sorted_list  # Return the sorted list
+
+
