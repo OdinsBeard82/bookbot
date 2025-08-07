06ad0ed352cf99994877163521b8aaa982856348
Daniel Charles <danielcharles_500@hotmail.com>
2025-08-07 17:14:32 +0100

    Arguments lesson complete

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, character_count, print_a_report
 
 def get_book_text(path):
@@ -5,12 +6,17 @@ def get_book_text(path):
         return f.read()
 
 def main():
-    result = get_book_text("books/frankenstein.txt")
+   if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+   else:
+    path = sys.argv[1]
+    result = get_book_text(path)  
     num_words = count_words(result)
     print(f"Found {num_words} total words")
 
     char_counts = character_count(result)
-
     report = print_a_report(char_counts)
     
     for item in report:
